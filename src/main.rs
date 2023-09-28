679d75af947f304c83c6efe6ace38926a1e6f20c
Ivan Polanis <polanisivan@gmail.com>
2023-09-28 09:29:37 -0300

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,78 @@
+use std::io::Write;
+
+enum Option {
+    Encrypt,
+    Decrypt,
+}
+fn menu() {
+    println!("Seleccione una opción:");
+    println!("1 - Desencriptar");
+    println!("0 - Encriptar");
+    println!("-1 - Salir");
+}
 fn main() {
-    let mut input = String::new();
+    loop {
+        menu();
+        let mut option = String::new();
 
-    match std::io::stdin().read_line(&mut input) {
-        Ok(_) => {}
-        Err(_e) => {
-            println!("Tu entrada no es valida");
+        std::io::stdin()
+            .read_line(&mut option)
+            .expect("No se puedo leer la entrada.");
+
+        let option: i32 = match option.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Entrada no válida. Por favor, ingrese un número válido.");
+                continue;
+            }
+        };
+
+        let action = match option {
+            1 => Option::Decrypt,
+            0 => Option::Encrypt,
+            -1 => break,
+            _ => {
+                println!("Opción no válida. Por favor, seleccione una opción válida.");
+                continue;
+            }
+        };
+
+        print!("Ingrese el texto: ");
+        std::io::stdout()
+            .flush()
+            .expect("Error al limpiar el búfer de salida.");
+        let mut input = String::new();
+
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Tu entrada no es valida.");
+
+        let input = parse_input(input);
+        match action {
+            Option::Encrypt => {
+                let pwd: [[f64; 3]; 3] = [[35.0, 53.0, 12.0], [12.0, 21.0, 5.0], [2.0, 4.0, 1.0]];
+                let result = to_number(&input, &pwd);
+
+                let mod_result = get_mod(&result);
+
+                let encrypted_msg = get_message(&mod_result);
+
+                println!("El mensaje encriptado es: {:?}", encrypted_msg);
+            }
+            Option::Decrypt => {
+                let invs: [[f64; 3]; 3] = [[1.0, 25.0, 13.0], [28.0, 11.0, 29.0], [6.0, 26.0, 9.0]];
+
+                let invs_result = to_number(&input, &invs);
+
+                let invs_mod = get_mod(&invs_result);
+
+                let decrypted_msg = get_message(&invs_mod);
+                println!("El mensaje desencriptado es: {:?}", decrypted_msg);
+            }
         }
+
+        let _invs: [[f64; 3]; 3] = [[1.0, -5.0, 13.0], [-2.0, 11.0, -31.0], [6.0, -34.0, 99.0]];
     }
-
-    let input = parse_input(input);
-
-    let pwd: [[f64; 3]; 3] = [[35.0, 53.0, 12.0], [12.0, 21.0, 5.0], [2.0, 4.0, 1.0]];
-
-    let result = to_number(&input, &pwd);
-
-    let mod_result = get_mod(&result);
-
-    let message = get_message(&mod_result);
-
-    let _invs: [[f64; 3]; 3] = [[1.0, -5.0, 13.0], [-2.0, 11.0, -31.0], [6.0, -34.0, 99.0]];
-
-    let invs: [[f64; 3]; 3] = [[1.0, 25.0, 13.0], [28.0, 11.0, 29.0], [6.0, 26.0, 9.0]];
-
-    let invs_result = to_number(&message, &invs);
-
-    println!("{:?}", message);
-
-    let invs_mod = get_mod(&invs_result);
-
-    let original = get_message(&invs_mod);
-    println!("{:?}", original);
 }
 
 fn parse_input(str: String) -> String {
